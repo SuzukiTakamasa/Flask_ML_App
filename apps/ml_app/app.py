586508b1c6c2e508550aa4841f9b9a7d10d4893e
Suzuki Takamasa <incubus.appalachia@gmail.com>
2022-05-11 00:23:49 +0900

    0410 before validation

--- apps/ml_app/app.py
+++ apps/ml_app/app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, current_app, g, request, redirect
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
 
+#TOP(index)画面
 @app.route("/")
 def index():
     return render_template("index.html", name="Piraruku")
 
-@app.route("/data_lists/<string:name>",
-          methods=["GET", "POST"],
-          endpoint="data_lists_endpoint")
+#一覧画面
+@app.route("/data_lists/<string:name>", methods=["GET", "POST"], endpoint="data_lists_endpoint")
 def data_lists(name):
     return "Here is some data, {}".format(name)
 
-with app.test_request_context():
-    print(url_for("index"))
-    print(url_for("data_lists_endpoint", name="Piraruku", page=1))
+#お問い合わせフォーム画面
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+#お問い合わせ完了画面
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+
+        #Send Email
+
+        return redirect(url_for("contact_complete"))
+
+    return render_template("contact_complete.html")
