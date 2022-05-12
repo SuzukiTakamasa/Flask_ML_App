b070362538e08ff9e683922ceade4c4d013d1614
Suzuki Takamasa <incubus.appalachia@gmail.com>
2022-05-12 23:55:18 +0900

    add mail sending function

--- apps/ml_app/app.py
+++ apps/ml_app/app.py
@@ -10,13 +10,24 @@ from flask import (
     flash,
 )
 import logging
+import os
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
 app.logger.setLevel(logging.DEBUG)
 app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 toolbar = DebugToolbarExtension(app)
+
+app.config["MAIL_SERVER"] = os.environ["MAIL_SERVER"]
+app.config["MAIL_PORT"] = os.environ["MAIL_PORT"]
+app.config["MAIL_USE_TLS"] = os.environ["MAIL_USE_TLS"]
+app.config["MAIL_USERNAME"] = os.environ["MAIL_USERNAME"]
+app.config["MAIL_PASSWORD"] = os.environ["MAIL_PASSWORD"]
+app.config["MAIL_DEFAULT_SENDER"] = os.environ["MAIL_DEFAULT_SENDER"]
+
+mail = Mail(app)
 
 #TOP(index)画面
 @app.route("/")
@@ -65,6 +76,13 @@ def contact_complete():
             return redirect(url_for("contact"))
 
         #Send Email
+        def send_email(to, subject, template, **kwargs):
+            msg = Message(subject, recipients=[to])
+            msg.body = render_template(template + ".txt", **kwargs)
+            msg.html = render_template(template + ".html", **kwargs)
+            mail.send(msg)
+
+        send_email(email,"Thank you for your contact", "contact_mail", username=username, description=description)
         flash("Thank you for your contact.")
         return redirect(url_for("contact_complete"))
 
