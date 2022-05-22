e41144252587c367f65517bb90f6fc9a0aa1ad0e
Suzuki Takamasa <incubus.appalachia@gmail.com>
2022-05-22 15:54:26 +0900

    add extends and config setting

--- apps/app.py
+++ apps/app.py
@@ -3,12 +3,15 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from apps.config import config
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
 
-def create_app():
+def create_app(config_key):
     app = Flask(__name__)
+
+    app.config.from_object(config[config_key])
 
     app.config.from_mapping(
         SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
@@ -22,7 +25,6 @@ def create_app():
 
     db.init_app(app)
     Migrate(app, db)
-
 
     from apps.crud import views as crud_views
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
---
+++ apps/config.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+basedir = Path(__file__).parents[1]
+
+class BasicConfig:
+    SECRET_KEY = "2AZSMss3p5QPbcY2hBsJ"
+    WTF_CSRF_SECRET_KEY = "AuwzyszUSsugKN7KZs6f"
+
+class LocalConfig(BasicConfig):
+    SQLALCHEMY_DATABASE_URI = f"sqlite:////{basedir}/local.sqlite"
+    SQLALCHEMY_TRACK_MODIFICATION = False
+    SQLALCHEMY_ECHO = True
+
+class TestingConfig(BasicConfig):
+    SQLALCHEMY_DATABASE_URI = f"sqlite:////{basedir}/testing.sqlite"
+    SQLALCHEMY_TRACK_MODIFICATION = False
+    WTF_CSRF_ENABLED = False
+
+config = {
+    "testing": TestingConfig,
+    "local": LocalConfig,
+}
