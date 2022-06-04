8e09ba924f4a9d1a149814654bf9ee0a874990ae
Suzuki Takamasa <incubus.appalachia@gmail.com>
2022-06-04 17:26:10 +0900

    add signup/signin css

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -44,7 +44,7 @@ def login():
 
         if user is not None and user.verify_password(form.password.data):
             login_user(user)
-            return redirect(url_for("crud.users"))
+            return redirect(url_for("detector.index"))
         
         flash("Email Address or password is incorrect.")
     return render_template("auth/login.html", form=form)
